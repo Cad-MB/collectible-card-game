e14803a4e282b2d7417e318b14429a55fca50137
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-10-11 00:18:12 +0200

    Contracts - creation des collections.

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -1,32 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "hardhat/console.sol";
 
-contract Collection is Ownable {
-  string public name;
+contract Collection is ERC721URIStorage, ERC721Enumerable, Ownable {
+  string public collectionName;
   int public cardCount;
-  string public collectionID;
-  uint256[] public cards;
-  address private _cardNftContract;
-   
-  constructor(string memory _name, string memory _collectionId, int _cardCount, address cardNftContract) Ownable(msg.sender) {
-    name = _name;
+  uint256 public tokenID = 0;
+
+  // mapping from account to tokenIds
+  mapping(address => uint256[]) public accountsTokens;
+
+  constructor(
+    address initialOwner,
+    string memory _name,
+    int _cardCount
+  ) Ownable(initialOwner) ERC721("PokemonCard", "PKMN") {
+    collectionName = _name;
     cardCount = _cardCount;
-    collectionID = _collectionId;
-    _cardNftContract = cardNftContract;
   }
 
-  function addCard(uint256 cardId) external onlyOwner {
-    // if cards.length is equal or higher than cardCount, it will revert
-    require(cards.length < uint256(cardCount), "Collection: Card count exceeded");
-    cards.push(cardId);
+  function mintNFT(
+    address recipient,
+    string memory newTokenURI
+  ) public onlyOwner returns (uint256) {
+    tokenID = tokenID + 1;
+
+    uint256 newItemId = tokenID;
+    _mint(recipient, newItemId); // Mint the token
+    _setTokenURI(newItemId, newTokenURI); // Set the token URI
+    return newItemId;
   }
 
-  function getCards() external view returns (uint256[] memory) {
-    return cards;
+  // Override required functions
+  function _increaseBalance(
+    address account,
+    uint128 initialValue
+  ) internal override(ERC721, ERC721Enumerable) {
+    super._increaseBalance(account, initialValue);
   }
 
-  function getCardNftContract() external view returns (address) {
-    return _cardNftContract;
+  function _update(
+    address to,
+    uint256 tokenId,
+    address auth
+  ) internal override(ERC721, ERC721Enumerable) returns (address) {
+    return super._update(to, tokenId, auth);
+  }
+
+  function supportsInterface(
+    bytes4 interfaceId
+  ) public view override(ERC721URIStorage, ERC721Enumerable) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
+
+  function tokenURI(
+    uint256 tokenId
+  ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    return super.tokenURI(tokenId);
+  }
+
+  function _baseURI() internal pure override(ERC721) returns (string memory) {
+    return "http://localhost:3030/getCard/";
   }
 }
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -1,17 +1,82 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 import "./Collection.sol";
 
-contract Main {
-  int private count;
+// thanks to TME's teammates for the collab
+contract Main is Ownable {
+  int private index;
   mapping(int => Collection) private collections;
+  mapping(string => Collection) private collectionsById;
 
-  constructor() {
-    count = 0;
+  // store the owners of the cards
+  address[] public owners;
+  mapping(address => bool) verifyOwner;
+
+  constructor() Ownable(0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199) {
+    index = 0;
   }
 
-  function createCollection(string calldata name, int cardCount) external {
-    collections[count++] = new Collection(name, cardCount);
+  function createCollection(
+    string calldata collentionName,
+    int cardCount,
+    string calldata id
+  ) external onlyOwner {
+    address initialOwner = address(this); // the contract itself is the owner
+    Collection newCollection = new Collection(
+      initialOwner,
+      collentionName,
+      cardCount
+    );
+    collections[index] = newCollection; // store the new collection
+    collectionsById[id] = newCollection; // store the new collection
+    index++; // increment the index
+  }
+
+  function mintCard(
+    string calldata collectionId,
+    address recipient,
+    string memory newTokenURI
+  ) external onlyOwner returns (uint256) {
+    bool theOwner = verifyOwner[recipient];
+    if (theOwner == false) {
+      owners.push(recipient);
+      verifyOwner[recipient] = true;
+    }
+    Collection collection = collectionsById[collectionId];
+    uint256 tokenId = collection.mintNFT(recipient, newTokenURI);
+    return tokenId;
+  }
+
+  // by copilot
+  function getOwners() public view returns (address[] memory) {
+    return owners;
+  }
+
+  // returns an array of the balance of an account in each collection
+  function balanceOf(address owner) public view returns (uint[] memory) {
+    uint[] memory balances = new uint[](uint(index));
+    for (uint i = 0; i < uint(index); i++) {
+      balances[i] = collections[int(i)].balanceOf(owner);
+    }
+    return balances;
+  }
+
+  // inspired by Joumana
+  function tokenOfOwnerByIndex(
+    int collectionId,
+    address owner,
+    uint i
+  ) external view returns (uint256) {
+    return collections[collectionId].tokenOfOwnerByIndex(owner, i);
+  }
+
+  function tokenURI(
+    int collectionId,
+    uint i
+  ) external view returns (string memory) {
+    return collections[collectionId].tokenURI(i);
   }
 }
