e5975b901ef171aaf1e618ef4dd9cd3d77e2b3ef
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-10-14 21:56:03 +0200

    Backend - fix owner ID. mint NFT work

--- backend/controllers/initServer.js
+++ backend/controllers/initServer.js
@@ -1,12 +1,19 @@
 const cors = require('cors');
 const express = require('express');
 const { fetchAndCreateNFTCollection } = require("../../contracts/connectors/Collections")
+const { mintNFT } = require("../../contracts/connectors/mintNFT")
 
 function initServer(app) {
     app.use(cors()); // enable CORS for all requests
     app.use(express.json()); // enable parsing JSON bodies
 
     fetchAndCreateNFTCollection();
+
+    app.get('/', async (req, res) => {
+        const response = await mintNFT("base1", '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199', 'mcd19-3');
+        res.send(response);
+    }
+    );
 
     require("../routes/routes")(app); // init the routes
 }
--- contracts/connectors/tokens.js
+++ contracts/connectors/tokens.js
@@ -5,7 +5,7 @@ const provider = new JsonRpcProvider('http://localhost:8545');
 const wallet = new ethers.Wallet('0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e', provider);
 
 // Load your NFT smart contract (replace with your contract address and ABI)
-const contractAddress = '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199';
+const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 const contract = require("../artifacts/src/Main.sol/Main.json");
 const contractABI = contract.abi; // Your contract's ABI
 const nftContract = new ethers.Contract(contractAddress, contractABI, wallet);
